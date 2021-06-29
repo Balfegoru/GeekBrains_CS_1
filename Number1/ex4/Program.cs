1f5bf21534e07784fd9036939259420e2c55766a
Balfegoru <duccoducco@gmail.com>
2021-06-29 18:16:07 +0300

    Домашняя работа 3

--- Number1/ex4/Program.cs
+++ Number1/ex4/Program.cs
@@ -8,8 +8,144 @@ namespace ex4
 {
     class Program
     {
+        //Работа в процессе реализации.Есть мысль попробовать случайное расположение кораблей
+
+        static void ClearField(string[,] field) //Чистое поле
+        {
+            for (int i = 0; i < field.GetLength(0); i++)
+            {
+                for (int j = 0; j < field.GetLength(1); j++)
+                {
+                    field[i, j] = "O";
+                }
+            }
+        }
+
+
+        
+
+        static void Randomazer(string[,] field, int numBoat) //Заполнение кораблями 
+        {
+            string x = "X";
+            int boats = numBoat;
+            Random randomer = new Random();
+
+            //4 палубный
+
+            while (true)
+            {
+                int i = randomer.Next(0, 11);
+                int j = randomer.Next(0, 11);
+                int ii = i;
+                int jj = j;
+
+                    if (field[i, j] != x)
+                    {
+                        field[i, j] = x;
+                        boats--;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+
+                    try
+                    {
+                        for (; boats > 0; boats--)
+                        {
+                            i++;
+                            if (field[i, j] == x)
+                            {
+                                field[i, j] = x;
+                            }
+                        }
+                    break;
+                    }
+                    catch
+                    {
+                        try
+                        {
+                        i = ii;
+                        boats = numBoat;
+
+                            for (; boats > 0; boats--)
+                            {
+                                i--;
+                                if (field[i, j] == x)
+                                {
+                                    field[i, j] = x;
+                                }
+
+                            }
+                        break;
+                        }
+                        catch
+                        {
+                            try
+                            {
+                            i = ii;
+                            boats = numBoat;
+                            for (; boats > 0; boats--)
+                                {
+                                    j++;
+                                    if (field[i, j] == x)
+                                    {
+                                        field[i, j] = x;
+                                    }
+                                }
+                            }
+                            catch
+                            {
+
+                                try
+                                {
+                                    j = jj;
+                                    i = ii;
+                                boats = numBoat;
+                                for (; boats > 0; boats--)
+                                        {
+                                            j--;
+                                            if (field[i, j] == x)
+                                            {
+                                            field[i, j] = x;
+                                            }
+                                        }
+                                    break;
+                                }
+                                catch
+                                {
+                                    continue;
+                                }
+                            }
+                        }
+                    }
+            }
+        }
+                
+     
+        static void Vision(string[,] field) //Отображение
+        {
+            for (int i = 0; i < field.GetLength(0); i++)
+            {
+                for (int j = 0; j < field.GetLength(1); j++)
+                {
+                    Console.Write(field[i,j]);
+                }
+                Console.WriteLine();
+            }
+
+        }
+
         static void Main(string[] args)
         {
+            string[,] field = new string[10, 10];
+
+            ClearField(field);
+            Vision(field);
+            Randomazer(field,4);
+            Console.WriteLine();
+            Vision(field);
         }
     }
 }
+
